1c1e1503706b0dc723881612925ed3bdd9900557
thanhkhang2312 <thanhkhang2312@gmail.com>
2015-04-18 15:36:44 +0300

    chuyen folder tu service sang controller

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,2 +1,8 @@
 angular.module('myApp.controller',[])
 
+.controller('StudentCtrl',function($scope,$http){
+	 $http.get("/userpost").success(function(response){
+	 	$scope.contents = response;
+	 	console.log(response);
+	 })
+})
--- public/js/service.js
+++ public/js/service.js
@@ -2,7 +2,7 @@ angular.module('myApp.service',[])
 
 .controller('StudentCtrl',function($scope,$http){
 	 $http.get("/userpost").success(function(response){
-	 	//$scope.contents = response.collection;
+	 	$scope.contents = response;
 	 	console.log(response);
 	 })
 })
