3a1f98dbf82119b95bf7f0065edefc36fa755c7b
Vi Elite <nmt1994@gmail.com>
2015-04-18 17:27:24 +0300

    gay

--- routes/index.js
+++ routes/index.js
@@ -23,57 +23,10 @@ router.get('/userpost', function(req, res) {
 });
 
 router.post('/newevent', function(req,res){
-<<<<<<< HEAD
-  var content;
-      var input = JSON.parse(req.body);
-    console.log(input);
-      var time = new Date();
-      var newEvent = new Event({content: input.content.toString(), image: input.image,date:time });
-      newEvent.save(function(err,data){
-          if(err) console.log(err);
-          else
-          {
-              console.log('Saved :' + data);
-              res.json(data);
-          }
-      });
-=======
-  //var content;
-    express.bodyParser();
-
-        var time = new Date();
-        var newEvent = new Event({content: req.body.content, image: req.body.image,date:time });
-
-        newEvent.save(function(err,data){
-            if(err) console.log(err);
-            else
-            {
-                console.log('Saved :' + data);
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.json(data);
-            }
-        });
-
-
->>>>>>> 59f82bc2226d4f106b268c6da3b2768e1396a0e0
-
-    res.end();
 
 });
 
 router.get('/getEvent',function(req,res){
-
-    Event.find({},function(err,docs){
-        if(err){
-            console.log(err);
-        }
-        else {
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.write(docs);
-
-        }
-    });
-    res.end();
 
 });
 
