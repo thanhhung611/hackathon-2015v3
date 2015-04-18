5cc89cfb82c6fe420c9cdb6b9ffcc8b265ab9f96
thanhhung611 <thanhhung.phamquang@gmail.com>
2015-04-18 14:52:36 +0300

    retrieve data
    get method

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ var fs = require('fs');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Expressssssss' });
 });
-
+/* GET Userlist page. */
+router.get('/userpost', function(req, res) {
+    var db = req.db;
+    var collection = [{ "username" : "testuser2", "email" : "testuser2@testdomain.com" }, { "username" : "testuser3", "email" : "testuser3@testdomain.com" }]
+    res.json(collection);
+});
 module.exports = router;
